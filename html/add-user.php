1907c42f0538a221215670b8e7ab53c075f908b5
Caleb Mabry <Caleb.mabry.15@cnu.edu>
2019-02-04 15:27:39 -0500

    Made a boo boo and fixed it

--- html/add-user.php
+++ html/add-user.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "root";
+$password = "root";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
