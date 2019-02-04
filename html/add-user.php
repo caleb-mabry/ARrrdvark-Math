f2e12977c7c40e17d44b78b4af2d60672c070370
Caleb Mabry <Caleb.mabry.15@cnu.edu>
2019-02-04 15:22:18 -0500

    If a column in the database is auto incremement, it does not need to be specified in the query

--- html/add-user.php
+++ html/add-user.php
@@ -11,8 +11,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "INSERT INTO users (ID, FirstName, LastName, Password, Email)
-VALUES (DEFAULT, $_POST["firstName"]','$_POST["lastName"]','$_POST["password"]','$_POST["email"]'";
+$sql = "INSERT INTO users (FirstName, LastName, Password, Email)
+VALUES ($_POST["firstName"]','$_POST["lastName"]','$_POST["password"]','$_POST["email"]'";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
