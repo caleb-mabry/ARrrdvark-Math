7b692e378cb5d75b38fc49523f89f578b7ed5644
Caleb Mabry <Caleb.mabry.15@cnu.edu>
2019-02-04 15:12:31 -0500

    Does this workkkk?

--- html/server.php
+++ html/server.php
@@ -1,39 +1,24 @@
 <?php
-	// Connect to the database
-	$db = new mysqli('localhost', 'root', 'root', 'arrrdvark_math');
-	if (isset($_POST['signup'])) {
-		$firstName = mysql_real_escape_string($_POST['firstName']);
-		$lastName = mysql_real_escape_string($_POST['lastName']);
-		$password = mysql_real_escape_string($_POST['password']);
-		$email = mysql_real_escape_string($_POST['email']);
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "arrrdvark_math";
 
-		// ensure that forms are filled out properly
-		if (empty($firstName)) {
-			array_push($errors, "firstName is Required"); //add error to errors array
-		}
-		if (empty($lastName)) {
-			array_push($errors, "lastName is Required"); //add error to errors array
-		}
-		if (empty($password)) {
-			array_push($errors, "password is Required"); //add error to errors array
-		}
-		if (empty($email)) {
-			array_push($errors, "email is Required"); //add error to errors array
-		}
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 
-		// If there are no errors, save user to database
-		if(count($errors) == 0) {
-			$password = md5($password); //Encrypt the password before it is stored into the database
-			$sql = "INSERT INTO users VALUES ('".$_POST['firstName']."','".$_POST['lastName']."','".$_POST['password']."','".$_POST['email']."')";
-		}
-	}
-			$password = md5($password); //Encrypt the password before it is stored into the database
-			$sql = "INSERT INTO users VALUES ('".$_POST['firstName']."','".$_POST['lastName']."','".$_POST['password']."','".$_POST['email']."')";
-	$result = $db->query($sql);
-	if ($result) {
-		echo "it worked fam";
-	}else {
-		echo "try again sucker";
-	}
+$sql = "INSERT INTO users (ID, FirstName, LastName, Password, Email)
+VALUES ('1','$_POST["firstName"]','$_POST["lastName"]','$_POST["password"]','$_POST["email"]'";
 
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
