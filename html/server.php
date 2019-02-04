3c833d8e00b4c560f023a9e2e1aa6051f33d7772
Caleb Mabry <Caleb.mabry.15@cnu.edu>
2019-02-04 15:23:07 -0500

    Probably would be a good idea to connect to the server first

--- html/server.php
+++ html/server.php
@@ -1,24 +1,14 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "arrrdvark_math";
+$username = "username";
+$password = "password";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password);
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
-$sql = "INSERT INTO users (ID, FirstName, LastName, Password, Email)
-VALUES ('1','$_POST["firstName"]','$_POST["lastName"]','$_POST["password"]','$_POST["email"]'";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
+echo "Connected successfully";
 ?>
