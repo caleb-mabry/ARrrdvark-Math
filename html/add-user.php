8ec49c3943a363a8cfe6416705b53351bd0c3b6a
Caleb Mabry <Caleb.mabry.15@cnu.edu>
2019-02-04 15:31:47 -0500

    Reverted things back. Clicking on the button makes the value not submit

--- html/add-user.php
+++ html/add-user.php
@@ -2,18 +2,17 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
+$dbname = "arrrdvark_math";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
-
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
 
 $sql = "INSERT INTO users (FirstName, LastName, Password, Email)
-VALUES ($_POST["firstName"]','$_POST["lastName"]','$_POST["password"]','$_POST["email"]'";
+VALUES (DEFAULT, $_POST["firstName"]','$_POST["lastName"]','$_POST["password"]','$_POST["email"]'";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
