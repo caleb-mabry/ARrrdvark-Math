27ca5c67ba94c739aaa649724981d72ccee8244a
Caleb Mabry <Caleb.mabry.15@cnu.edu>
2019-02-04 15:26:44 -0500

    Testing a theory 2

--- html/add-user.php
+++ html/add-user.php
@@ -1,15 +1,16 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "arrrdvark_math";
+$username = "username";
+$password = "password";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password);
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
+echo "Connected successfully";
 
 $sql = "INSERT INTO users (FirstName, LastName, Password, Email)
 VALUES ($_POST["firstName"]','$_POST["lastName"]','$_POST["password"]','$_POST["email"]'";
