d8aa7aabe59eafb10d1224ab6b7b28df39e94aae
Caleb Mabry <Caleb.mabry.15@cnu.edu>
2019-02-05 11:10:16 -0500

    Changed from single to double quote

--- html/server.php
+++ html/server.php
@@ -7,11 +7,11 @@ $dbname = "arrrdvark_math";
 // Create connection
 $conn = new mysqli($servername, $serverusername, $serverpassword, $dbname);
 
-if (isset($_POST['signup'])) {
-	$firstName = $_POST['firstName'];
-	$lastName = $_POST['lastName'];
-	$password = $_POST['password'];
-	$email = $_POST['email'];
+if (isset($_POST["signup"])) {
+	$firstName = $_POST["firstName"];
+	$lastName = $_POST["lastName"];
+	$password = $_POST["password"];
+	$email = $_POST["email"];
 	$sql = "INSERT INTO users (FirstName, LastName, Password, Email) VALUES ('$firstName','$lastName','$password','email')";
 	if ($conn->query($sql) === TRUE) {
 		echo "New record created successfully";
