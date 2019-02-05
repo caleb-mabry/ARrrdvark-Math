1a9842b780180792dcfb63ed6481cf2606462e18
Caleb Mabry <Caleb.mabry.15@cnu.edu>
2019-02-04 20:34:37 -0500

    Still testing

--- html/add-user.php
+++ html/add-user.php
@@ -10,9 +10,11 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
+if (isset($_POST['signup'])) {
 $sql = "INSERT INTO users (FirstName, LastName, Password, Email)
 VALUES (DEFAULT, $_POST["firstName"]','$_POST["lastName"]','$_POST["password"]','$_POST["email"]'";
+query($sql);
+}
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
--- html/server.php
+++ html/server.php
@@ -2,13 +2,17 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
+$dbname = "arrrdvark_math";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
-
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
+if (isset($_POST[''])) {
+$sql = "INSERT INTO users (FirstName, LastName, Password, Email)
+VALUES (DEFAULT, $_POST["firstName"]','$_POST["lastName"]','$_POST["password"]','$_POST["email"]'";
+}
+$conn->close();
 ?>
