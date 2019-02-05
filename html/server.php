a1043bdbba57524ec8f2651a17e864c529108d96
Caleb Mabry <Caleb.mabry.15@cnu.edu>
2019-02-04 21:34:41 -0500

    This better work.

--- html/server.php
+++ html/server.php
@@ -7,29 +7,14 @@ $dbname = "arrrdvark_math";
 // Create connection
 $conn = new mysqli($servername, $serverusername, $serverpassword, $dbname);
 
-	if (empty($_POST["firstName"])) {
-		$firstNameErr = "First name is required";
-	} else {
-		$firstName = $_POST["firstName"];
-	}
-	
-	if (empty($_POST["lastName"])) {
-		$lastNameErr = "Last name is required";
-	} else {
-		$lastName = $_POST["lastName"];
-	}
-	
-	if (empty($_POST["password"])) {
-		$passwordErr = "Password is required";
-	} else {
-		$password = $_POST["password"];
-	}
 
-	if (empty($_POST["email"])) {
-		$emailErr = "Email is required"
-	} else {
+		$firstName = $_POST["firstName"];
+
+		$lastName = $_POST["lastName"];
+
+		$password = $_POST["password"];
+
 		$email = $_POST["email"];
-	}
 	$sql = "INSERT INTO users (FirstName, LastName, Password, Email) VALUES ('$firstName','$lastName','$password','$email')";
 	if ($conn->query($sql) === TRUE) {
 		echo "New record created successfully";
