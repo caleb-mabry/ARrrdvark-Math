ac63d2c0f03859d079df6af851e60f99871dbc25
Caleb Mabry <Caleb.mabry.15@cnu.edu>
2019-02-04 21:46:39 -0500

    This should work.

--- html/server.php
+++ html/server.php
@@ -6,35 +6,13 @@ $dbname = "arrrdvark_math";
 
 // Create connection
 $conn = new mysqli($servername, $serverusername, $serverpassword, $dbname);
-$firstNameErr = $lastNameErr = $passwordErr = $emailErr = "";
-$firstName = $lastName = $password = $email = "";
+
 if (isset($_POST['signup'])) {
-	if (empty($_POST['firstName'])) {
-		$firstNameErr = "First name is required";
-	} else {
-		$firstName = $_POST['firstName'];
-	}
-	
-	if (empty($_POST['lastName'])) {
-		$lastNameErr = "Last name is required";
-	} else {
-		$lastName = $_POST['lastName'];
-	}
-	
-	if (empty($_POST['password'])) {
-		$passwordErr = "Password is required";
-	} else {
-		$password = $_POST["password"];
-	}
-
-	if (empty($_POST['email'])) {
-		$emailErr = "Email is required"
-	} else {
-		$email = $_POST['email'];
-	}
-
-
-	$sql = "INSERT INTO users (FirstName, LastName, Password, Email) VALUES ('$firstName','$lastName','$password','$email')";
+	$firstName = $_POST['firstName'];
+	$lastName = $_POST['lastName'];
+	$password = $_POST['password'];
+	$email = $_POST['email'];
+	$sql = "INSERT INTO users (FirstName, LastName, Password, Email) VALUES ('$firstName','$lastName','$password','email')";
 	if ($conn->query($sql) === TRUE) {
 		echo "New record created successfully";
 	} else {
