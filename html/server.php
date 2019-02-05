f362cd615819693cb423a72adf5db22197d77c98
Caleb Mabry <Caleb.mabry.15@cnu.edu>
2019-02-04 20:54:30 -0500

    It now adds values to the database. But it's the wrong values

--- html/server.php
+++ html/server.php
@@ -8,11 +8,11 @@ $dbname = "arrrdvark_math";
 $conn = new mysqli($servername, $serverusername, $serverpassword, $dbname);
 
 if (isset($_POST['signup'])) {
-	$firstName = $_POST['firstName'];
-	$lastName = $_POST['lastName'];
-	$password = $_POST['password'];
-	$email = $_POST['email'];
-	$sql = "INSERT INTO users (FirstName, LastName, Password, Email) VALUES ('$firstName','$lastName','$password','email')";
+	$firstName = d;
+	$lastName = d;
+	$password = d;
+	$email = d;
+	$sql = "INSERT INTO users (FirstName, LastName, Password, Email) VALUES ('$_POST['firstName']','$_POST['lastName']','$_POST['password']','$_POST['email']";
 	if ($conn->query($sql) === TRUE) {
 		echo "New record created successfully";
 	} else {
