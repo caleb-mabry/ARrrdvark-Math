62e3d1822674a36dc69528bf84b8b1382293cfe0
Caleb Mabry <Caleb.mabry.15@cnu.edu>
2019-02-04 21:37:31 -0500

    I swear.

--- html/server.php
+++ html/server.php
@@ -6,15 +6,33 @@ $dbname = "arrrdvark_math";
 
 // Create connection
 $conn = new mysqli($servername, $serverusername, $serverpassword, $dbname);
-
-
-		$firstName = $_POST["firstName"];
-
-		$lastName = $_POST["lastName"];
-
+$firstNameErr = $lastNameErr = $passwordErr = $emailErr = "";
+if (isset($_POST['signup'])) {
+	if (empty($_POST['firstName'])) {
+		$firstNameErr = "First name is required";
+	} else {
+		$firstName = $_POST['firstName'];
+	}
+	
+	if (empty($_POST['lastName'])) {
+		$lastNameErr = "Last name is required";
+	} else {
+		$lastName = $_POST['lastName'];
+	}
+	
+	if (empty($_POST['password'])) {
+		$passwordErr = "Password is required";
+	} else {
 		$password = $_POST["password"];
+	}
 
-		$email = $_POST["email"];
+	if (empty($_POST['email'])) {
+		$emailErr = "Email is required"
+	} else {
+		$email = $_POST['email'];
+	}
+}
+
 	$sql = "INSERT INTO users (FirstName, LastName, Password, Email) VALUES ('$firstName','$lastName','$password','$email')";
 	if ($conn->query($sql) === TRUE) {
 		echo "New record created successfully";
