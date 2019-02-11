963090670334900bce0d45f545e3cbe6dc64a625
Caleb Mabry <Caleb.mabry.15@cnu.edu>
2019-02-11 10:40:58 -0500

    Adding a modal to the signup page for terms of service - testing

--- html/signup.php
+++ html/signup.php
@@ -84,21 +84,47 @@
 									<input type="password" class="form-control" name="password" aria-describedby="password" placeholder="Password">
 								</div>
 								<div class="form-check">
-								<input class="form-check-input" type="checkbox" name="emailAgree" value="" id="defaultCheck1">
-								<label class="form-check-label" for="defaultCheck1">
-									I <b>want</b> to recieve e-mails regarding updates or new products
-								</label>
-							</div>
-							<div class="form-check">
-								<input class="form-check-input" type="checkbox" name="termsAgree" value="" id="defaultCheck1">
-								<label class="form-check-label" for="defaultCheck1">
-									I Agree to the <a href="#">Terms of Service</a> Policy
-								</label>
-							</div>
-							<br>
-							<div class="form-group text-center-button">
-								<button class="btn btn-primary" type="submit" name="signup">Sign Up</button>
-							</div>
+									<input class="form-check-input" type="checkbox" name="emailAgree" value="" id="defaultCheck1">
+									<label class="form-check-label" for="defaultCheck1">
+										I <b>want</b> to recieve e-mails regarding updates or new products
+									</label>
+								</div>
+								<div class="form-check">
+									<input class="form-check-input" type="checkbox" name="termsAgree" value="" id="defaultCheck1">
+									<label class="form-check-label" for="defaultCheck1">
+										I Agree to the 
+										<!-- Button trigger modal -->
+										<a href="#" data-toggle="modal" data-target="#exampleModal">
+											terms of service
+										</a>
+
+										<!-- Modal -->
+										<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+											<div class="modal-dialog" role="document">
+												<div class="modal-content">
+													<div class="modal-header">
+														<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+														<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+															<span aria-hidden="true">&times;</span>
+														</button>
+													</div>
+													<div class="modal-body">
+														...
+													</div>
+													<div class="modal-footer">
+														<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+														<button type="button" class="btn btn-primary">Save changes</button>
+													</div>
+												</div>
+											</div>
+										</div>
+										Policy
+									</label>
+								</div>
+								<br>
+								<div class="form-group text-center-button">
+									<button class="btn btn-primary" type="submit" name="signup">Sign Up</button>
+								</div>
 							</form>
 							
 
@@ -112,13 +138,13 @@
 	</div>
 </div>
 
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
-	<script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+<script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
 
 <script>
 	particlesJS("particles-js", {
