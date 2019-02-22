c48491cb01c277f84f0162d0dfd152eb5acd0b2a
Caleb Mabry <Caleb.mabry.15@cnu.edu>
2019-02-22 08:42:55 -0500

    Copied the code from the site and edited variables

--- html/login-process.php
+++ html/login-process.php
@@ -1,16 +1,15 @@
 <?php
 include('db-connect.php');
 
-$sql = "SELECT
-			email
-		FROM
-			users
-		WHERE
-			email = $_POST["emailLogin"]
-		AND
-			user_pass = sha1($_POST["passwordLogin"])";
-
+$sql = "SELECT 
+email
+FROM
+users
+WHERE
+email = '" . mysql_real_escape_string($_POST['emailLogin']) . "'
+AND
+password = '" . sha1($_POST['passwordLogin']) . "'";
 $conn->query($sql);
 
-	header("Location: https://ardvarklearning.net/forum.php")
-	?>
+header("Location: https://ardvarklearning.net/forum.php")
+?>
