9f6e41bd74f24b0567b2acc7bb762172e6b2fbbb
Caleb Mabry <Caleb.mabry.15@cnu.edu>
2019-02-22 08:29:20 -0500

    Changed the way that I queried the server

--- html/login-process.php
+++ html/login-process.php
@@ -10,12 +10,12 @@ $sql = "SELECT
 		AND
 			user_pass = sha1($_POST['user_pass'])";
 
-$result = $conn ->query($sql);
+$result = mysqli_query($sql);
 if(!$result) {
 	ecoh 'Something went wrong while signing in. Please try again later';
 }
 else {
 	$_SESSION['signed_in'] = true;
 	$_SESSION['user_name'] = $_POST['email'];
-	header("Location: https://ardvarklearning.net/login.php")
+	header("Location: https://ardvarklearning.net/forum.php")
 }
