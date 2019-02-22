86bf9136f10d33bbecebc157e41c89b05dd96df5
Caleb Mabry <Caleb.mabry.15@cnu.edu>
2019-02-22 08:49:13 -0500

    Fuck.

--- html/login-process.php
+++ html/login-process.php
@@ -1,14 +1,7 @@
 <?php
 include('db-connect.php');
 
-$sql = "SELECT 
-email
-FROM
-users
-WHERE
-email = '" . mysql_real_escape_string($_POST['emailLogin']) . "'
-AND
-password = '" . sha1($_POST['passwordLogin']) . "'";
+$sql = "SELECT * FROM 'users' WHERE email=$_POST["$emailLogin"] AND password='".sha1($_POST["$passwordLogin"])."'";
 $conn->query($sql);
 
 header("Location: https://ardvarklearning.net/forum.php")
