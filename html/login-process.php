b716a10e75fda8945851ba55a80e1cd808078f60
Caleb Mabry <Caleb.mabry.15@cnu.edu>
2019-02-23 19:57:15 -0500

    Cleaning things up while drunk. Changed login.php to login2.php and trying to implement a new STYLE of things

--- html/login-process.php
+++ html/login-process.php
@@ -10,8 +10,9 @@
                     AND
                         password = sha1($_POST['emailPassword'])";
 $result = $conn->query($sql);
-      if(!$result)      {
-                //something went wrong, display the error
+      if(!$result)
+      {
+           //something went wrong, display the error
                 echo 'Something went wrong while signing in. Please try again later.';
                 echo mysql_error(); //debugging purposes, uncomment when needed
             }
--- html/login.php
+++ html/login.php
@@ -1,13 +1,5 @@
-<?php
-   if(isset($_SESSION['signed_in']) && $_SESSION['signed_in'] == true) 
-   {
-   	echo 'This user is already signed in';
-   }
-   else {
-   	echo 'nuttt';
-   }
+<?php include('db-connect.php');
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -22,26 +14,12 @@
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
-<!-- Link to our stylesheet DO NOT MOVE -->
-<link rel="stylesheet" href="stylesheet.css">
+	<!-- Link to our stylesheet DO NOT MOVE -->
+	<link rel="stylesheet" href="stylesheet.css">
 
-	<style type="text/css">
-	@font-face {
-		font-family: vanillaregular;
-		src: url(Vanilla-webfont.woff);
-	}
-
-	h1, a{
-		font-family:vanillaregular;
-	}
-
-</style>
-
-<title>ARdvark Learning | Login</title>
-
-
-
+	<title>ARdvark Learning | Login</title>
 </head>
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
 	<a class="navbar-brand" href="index.html">ARdvark Learning</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -75,47 +53,28 @@
 </nav>
 <body class="moving-backgrounds">
 	<div id="particles-js"></div>
-	<div class="container">
-		<div class="row">
-			<div class="col">
-				
+	<div class="container-fluid text-center" style="font-family: vanillaregular; color: white; padding-top: 5%;">
+		<h1>Login</h1>
+		<br>
+		<div class="row text-center">
+			<div class="col-4">
+
 			</div>
+			<div class="col-4 rounded" style="background-color: #323E78; padding-top: 3%;">
 
-			<div class="col-8" style="padding-top: 12%">
-				<div class="row">
-
-					
-					<div class="col-6 rounded" style="color: #7D86B2;background-color: #323E78; padding-top: 20%;border-right: 1px dashed #ccc">
-						<p class="text-center"><b>New User?</b><br>
-						Create an ARdvark Account</p>
-						<div class="form-group text-center">
-							<a class="btn btn-primary" href="signup.php" role="button">Sign Up</a>
-						</div>
+				<form>
+					<div class="form-group">
+						<input type="email" class="form-control" name="loginEmail" placeholder="Enter email">
 					</div>
-					<div class="col-6 rounded" style="color: #7D86B2;background-color:  #323E78; padding-top: 5%">
-						<h3>Login</h3>
-						<form method="post" action="login-process.php">
-							<div class="form-group">
-								<label for="exampleInputEmail1">Email address</label>
-								<input type="email" name="emailLogin" class="form-control" aria-describedby="emailHelp" placeholder="Enter email">
-							</div>
-							<div class="form-group">
-								<label for="exampleInputPassword1">Password</label>
-								<input type="password" name="passwordLogin" class="form-control" placeholder="Password">
-							</div>
-							<a href="forgotpassword.html">Forgot password?</a>
-							<br>
-							<br>
-							<button type="submit" name="loginSubmit" class="btn btn-primary">Submit</button>
-
-						</form>
-						<br>
-						<br>
+					<div class="form-group">
+						<input type="password" class="form-control" name="loginPassword" placeholder="Enter Password">
 					</div>
-				</div>
+					<button type="submit" class="btn btn-primary">Submit</button>
+				</form>
+				<br>
 			</div>
-			<div class="col">
-				
+			<div class="col-3">
+
 			</div>
 		</div>
 	</div>
@@ -132,63 +91,63 @@
 	</script>
 
 	<script>
-	particlesJS("particles-js", {
-		particles: {
-			number: { value: 110, density: { enable: true, value_area: 800 } },
-			color: { value: "#afb63e" },
-			shape: {
-				type: "star",
-				stroke: { width: 0, color: "#000000" },
-				polygon: { nb_sides: 5 },
-				image: { src: "img/github.svg", width: 100, height: 100 }
+		particlesJS("particles-js", {
+			particles: {
+				number: { value: 110, density: { enable: true, value_area: 800 } },
+				color: { value: "#afb63e" },
+				shape: {
+					type: "star",
+					stroke: { width: 0, color: "#000000" },
+					polygon: { nb_sides: 5 },
+					image: { src: "img/github.svg", width: 100, height: 100 }
+				},
+				opacity: {
+					value: 0.5,
+					random: false,
+					anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+				},
+				size: {
+					value: 3,
+					random: false,
+					anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+				},
+				line_linked: {
+					enable: true,
+					distance: 150,
+					color: "#28219b",
+					opacity: 0.4,
+					width: 1
+				},
+				move: {
+					enable: true,
+					speed: 0.75,
+					direction: "bottom",
+					random: false,
+					straight: false,
+					out_mode: "bounce",
+					bounce: false,
+					attract: { enable: false, rotateX: 600, rotateY: 1200 }
+				}
 			},
-			opacity: {
-				value: 0.5,
-				random: false,
-				anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+			interactivity: {
+				detect_on: "canvas",
+				events: {
+					onhover: { enable: false, mode: "repulse" },
+					onclick: { enable: true, mode: "push" },
+					resize: true
+				},
+				modes: {
+					grab: { distance: 400, line_linked: { opacity: 1 } },
+					bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+					repulse: { distance: 200, duration: 0.4 },
+					push: { particles_nb: 4 },
+					remove: { particles_nb: 2 }
+				}
 			},
-			size: {
-				value: 3,
-				random: false,
-				anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
-			},
-			line_linked: {
-				enable: true,
-				distance: 150,
-				color: "#28219b",
-				opacity: 0.4,
-				width: 1
-			},
-			move: {
-				enable: true,
-				speed: 0.75,
-				direction: "bottom",
-				random: false,
-				straight: false,
-				out_mode: "bounce",
-				bounce: false,
-				attract: { enable: false, rotateX: 600, rotateY: 1200 }
-			}
-		},
-		interactivity: {
-			detect_on: "canvas",
-			events: {
-				onhover: { enable: false, mode: "repulse" },
-				onclick: { enable: true, mode: "push" },
-				resize: true
-			},
-			modes: {
-				grab: { distance: 400, line_linked: { opacity: 1 } },
-				bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-				repulse: { distance: 200, duration: 0.4 },
-				push: { particles_nb: 4 },
-				remove: { particles_nb: 2 }
-			}
-		},
-		retina_detect: true
-	});
+			retina_detect: true
+		});
 
-	requestAnimationFrame(update);
-</script>
+		requestAnimationFrame(update);
+	</script>
 </body>
 </html>
